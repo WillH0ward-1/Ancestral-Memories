ffb9415686d61a63f05fc314a5a962949ff2a1da
Will Howard <92125591+WillH0ward-1@users.noreply.github.com>
2023-08-18 16:17:30 +0100

    IPA integration WIP

--- Ancestral Memories Master/Assets/CsPhonetics-master/Phonetics/src/IPA.cs
+++ Ancestral Memories Master/Assets/CsPhonetics-master/Phonetics/src/IPA.cs
@@ -5,34 +5,54 @@ using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using UnityEngine; // Needed for Application.dataPath
+using System.Linq;
 
 namespace Qkmaxware.Phonetics
 {
     public class IPA
     {
-
         private Dictionary<string, string> dictionary;
+        private HashSet<string> ipaSymbols = new HashSet<string>();
 
-        // Default constructor with hardcoded path
+        private static readonly string ipaIndexRawPath = Path.Combine(Application.dataPath, "LanguageGen", "CharResources");
+
         public IPA()
         {
             this.dictionary = new Dictionary<string, string>();
 
-            // Combine the paths to get the correct file path
             string filePath = Path.Combine(Application.dataPath, "CsPhonetics-master", "Phonetics", "data", "CMU.in.IPA.txt");
 
             foreach (string line in File.ReadAllLines(filePath))
             {
                 var parts = line.Split(',', 2);
                 if (parts.Length == 2)
+                {
                     dictionary[parts[0].Trim()] = parts[1].Trim();
+                    ExtractSymbols(parts[1].Trim());
+                }
+            }
+
+            WriteSymbolsToFile();
+            GenerateFormantFile();
+        }
+
+        private void ExtractSymbols(string transcription)
+        {
+            foreach (var character in transcription)
+            {
+                // Only add characters that are not whitespace to the set
+                if (!char.IsWhiteSpace(character))
+                {
+                    ipaSymbols.Add(character.ToString());
+                }
             }
         }
 
-        // Constructor to accept a different dictionary
-        public IPA(Dictionary<string, string> dictionary)
+
+        private void WriteSymbolsToFile()
         {
-            this.dictionary = dictionary;
+            string outputFilePath = Path.Combine(ipaIndexRawPath, "IPAindex.txt");
+            File.WriteAllLines(outputFilePath, ipaSymbols.OrderBy(s => s));
         }
 
         public string EnglishToIPA(string text)
@@ -53,6 +73,39 @@ namespace Qkmaxware.Phonetics
                 }
             }
             return builder.ToString();
+        }
+
+        private Dictionary<string, string[]> formantDictionary = new Dictionary<string, string[]>
+        {
+            { "a", new[] { "800", "1300", "2800" } },   // Mock values for example
+            { "ɑ", new[] { "700", "1150", "2600" } },   // General American 'father'
+            { "æ", new[] { "650", "1700", "2400" } },   // General American 'cat'
+            { "e", new[] { "400", "2300", "3000" } },   // Close to 'face'
+            { "ɛ", new[] { "600", "1800", "2500" } },   // General American 'bet'
+            { "ɚ", new[] { "500", "1500", "2500" } },   // R-colored 'nurse', this is a rough estimate
+            { "i", new[] { "300", "2400", "3000" } },   // General American 'fleece'
+            { "ɪ", new[] { "400", "2200", "2900" } },   // General American 'kit'
+            { "o", new[] { "450", "800", "2600" } },    // Close to 'goat'
+            { "ɔ", new[] { "500", "1000", "2500" } },   // General American 'thought'
+            { "u", new[] { "300", "800", "2400" } },    // General American 'goose'
+            { "ʊ", new[] { "400", "900", "2500" } },    // General American 'foot'
+            { "ʌ", new[] { "600", "1200", "2400" } }    // General American 'strut'
+        };
+
+        public void GenerateFormantFile()
+        {
+            string formantFilePath = Path.Combine(ipaIndexRawPath, "IPAFormants.txt");
+
+            List<string> lines = new List<string>();
+            foreach (var symbol in ipaSymbols)
+            {
+                if (formantDictionary.ContainsKey(symbol))
+                {
+                    var line = $"{symbol}: {string.Join(",", formantDictionary[symbol])}";
+                    lines.Add(line);
+                }
+            }
+            File.WriteAllLines(formantFilePath, lines);
         }
     }
 }
--- Ancestral Memories Master/Assets/FormantSynthesizer.cs
+++ Ancestral Memories Master/Assets/FormantSynthesizer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class FormantSynthesizer : MonoBehaviour
@@ -18,6 +19,8 @@ public class FormantSynthesizer : MonoBehaviour
     private void Start()
     {
         LoadPhoneticData();
+        LoadFormantData();
+
     }
 
     private void LoadPhoneticData()
@@ -51,6 +54,54 @@ public class FormantSynthesizer : MonoBehaviour
         }
     }
 
+    private Dictionary<string, List<int>> formantFrequencies = new Dictionary<string, List<int>>();
+
+    private void LoadFormantData()
+    {
+        string formantFilePath = Path.Combine(Application.dataPath, "LanguageGen", "CharResources", "IPAFormants.txt");
+
+        if (!File.Exists(formantFilePath))
+        {
+            Debug.LogError("IPAFormants.txt file not found.");
+            return;
+        }
+
+        string[] lines = File.ReadAllLines(formantFilePath);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(':');
+            if (parts.Length == 2)
+            {
+                string phoneme = parts[0].Trim();
+                List<int> frequencies = parts[1].Split(',').Select(int.Parse).ToList();
+                formantFrequencies[phoneme] = frequencies;
+            }
+        }
+    }
+
+    public void Speak(string word)
+    {
+        if (phoneticData.TryGetValue(word, out PhonemeInfo phonemeInfo))
+        {
+            foreach (string phoneme in phonemeInfo.phonemes)
+            {
+                if (formantFrequencies.TryGetValue(phoneme, out List<int> freqs))
+                {
+                    SendFrequenciesToCsoundInstrument(freqs);
+                }
+                else
+                {
+                    Debug.LogWarning($"No formant data found for phoneme: {phoneme}");
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"No phonetic data found for word: {word}");
+        }
+    }
+
     private void SendFrequenciesToCsoundInstrument(List<int> frequencies)
     {
         if (frequencies.Count >= 3 && csoundUnity != null)
@@ -63,25 +114,6 @@ public class FormantSynthesizer : MonoBehaviour
             string scoreEvent = $"i 1 0 1 {frequencies[0]} {frequencies[1]} {frequencies[2]}";
             csoundUnity.SendScoreEvent(scoreEvent);
         }
-    }
-
-    private int GetSyllableCount(string[] phonemeData)
-    {
-        // Implement this method to get the syllable count from the phoneme data
-        return 0;
-    }
-
-    private List<string> GetStressPattern(string[] phonemeData)
-    {
-        // Implement this method to get the stress pattern from the phoneme data
-        return new List<string>();
-    }
-
-    public void Speak(string word)
-    {
-        List<int> frequncies = new List<int>();
-
-        SendFrequenciesToCsoundInstrument(frequncies);
     }
 
     private struct PhonemeInfo
